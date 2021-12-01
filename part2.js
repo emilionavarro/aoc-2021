80e9f8297a23fa669e6f8ec22faf416d1e57911f
unknown <emilio.mst@gmail.com>
2021-12-01 16:02:25 -0600

    adds day 1, part 2

--- part2.js
+++ part2.js
@@ -1,6 +1,10 @@
-const depths = require('./input.json');
+const depths = require('./input-part2.json');
 
-const increasedQuantity = depths.reduce((acc, curr, index, allDepths) =>
-    (index === 0 || curr <= allDepths[index - 1]) ? acc : acc + 1, 0);
+const increasedQuantity = depths.reduce((acc, curr, index, allDepths) => {
+    if (index + 2 === allDepths.length - 1) return acc;
+    const currSum = curr + allDepths[index + 1] + allDepths[index + 2];
+    const nextSum = allDepths[index + 1] + allDepths[index + 2] + allDepths[index + 3];
+    return nextSum > currSum ? acc + 1 : acc;
+}, 0);
 
 console.log(increasedQuantity);
