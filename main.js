fadd83b90dd5588db2a2a3a76cef4a2154e041b5
unknown <emilio.mst@gmail.com>
2021-12-01 15:23:36 -0600

    part 1

---
+++ main.js
@@ -0,0 +1,5 @@
+const depths = require('./input.json');
+
+const increasedQuantity = depths.reduce((acc, curr, index, allDepths) => (index === 0 || curr <= allDepths[index - 1]) ? acc : acc + 1, 0);
+
+console.log(increasedQuantity);
